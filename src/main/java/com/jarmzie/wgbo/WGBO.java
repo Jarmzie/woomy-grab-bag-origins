8260a95c6b650fda7638d3ffa174673b6332ab97
Jarmzie <jna0420@aol.com>
2022-05-31 14:33:43 -0500

    Initial Commit

---
+++ src/main/java/com/jarmzie/wgbo/WGBO.java
@@ -0,0 +1,13 @@
+package com.jarmzie.wgbo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WGBO implements ModInitializer {
+
+    public static final String MOD_ID = "wgbo";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
